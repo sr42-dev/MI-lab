94920470dc461ed9149508d7ebf4983a7e4a5693
Sriram Radhakrishna <sriram.radhakrishna42@gmail.com>
2022-08-19 09:48:46 +0530

    Initialized week2.

---
+++ week2/PES1UG20CS435.py
@@ -0,0 +1,34 @@
+"""
+You can create any other helper funtions.
+Do not modify the given functions
+"""
+
+
+def A_star_Traversal(cost, heuristic, start_point, goals):
+    """
+    Perform A* Traversal and find the optimal path 
+    Args:
+        cost: cost matrix (list of floats/int)
+        heuristic: heuristics for A* (list of floats/int)
+        start_point: Staring node (int)
+        goals: Goal states (list of ints)
+    Returns:
+        path: path to goal state obtained from A*(list of ints)
+    """
+    path = []
+    # TODO
+    return path
+
+
+def DFS_Traversal(cost, start_point, goals):
+    """
+    Perform DFS Traversal and find the optimal path 
+        cost: cost matrix (list of floats/int)
+        start_point: Staring node (int)
+        goals: Goal states (list of ints)
+    Returns:
+        path: path to goal state obtained from DFS(list of ints)
+    """
+    path = []
+    # TODO
+    return path
